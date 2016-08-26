3393a78af194e3fe2db61f8b3d018a599270b796
Simon Hausmann <simon.hausmann@theqtcompany.com>
2016-08-26 11:59:58 +0200

    Improve robustness of QML plugin loading
    Use a variable that expands to the latest plugin version instead of
    hard-coding the string.
    
    Change-Id: Ica08496d3a9a357db32d0cd1e0abc8503c575c2e
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

--- src/imports/winextras/plugin.cpp
+++ src/imports/winextras/plugin.cpp
@@ -61,7 +61,7 @@ QT_BEGIN_NAMESPACE
 class QWinExtrasQmlPlugin : public QQmlExtensionPlugin
 {
     Q_OBJECT
-    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QQmlExtensionInterface/1.0")
+    Q_PLUGIN_METADATA(IID QQmlExtensionInterface_iid)
 
 public:
     QWinExtrasQmlPlugin(QObject *parent = 0) : QQmlExtensionPlugin(parent) { initResources(); }
